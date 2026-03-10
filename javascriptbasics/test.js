5b1319c7f7df5ae9c711a0fb65b9ce4361fcc251
workforarnav-11 <workforarnav.shukla11@gmail.com>
2026-03-10 20:09:32 +0000

    new

--- javascriptbasics/test.js
+++ javascriptbasics/test.js
@@ -1,1 +1,27 @@
-console.log("Arnav")
+const accountId = 27
+let accountEmail = "arnav.shukla11@gmail.com"
+var accountPassword = "234568"
+let accountCity = "Allahabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(typeof(accountEmail))
+const name = "Arnav Shukla"
+let repocount = 100
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const myName = new String('arnav shukla is a great footballer')
+
+console.log(myName.length);
+console.log(myName.toUpperCase());
+const balance = new Number()
+console.log(balance);
+console.log(typeof(balance));
+console.log(balance.toString().length);
+balance = 11234.5689
+console.log(balance.toFixed(5));
+
+
+
+
+
